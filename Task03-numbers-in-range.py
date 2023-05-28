337ce68bb5e92dfccf6d44cf8cd5509fce1abe99
R-Jain-72 <129484277+R-Jain-72@users.noreply.github.com>
2023-05-28 11:07:56 +0530

    Add files via upload

---
+++ Task03-numbers-in-range.py
@@ -0,0 +1,11 @@
+list1 = [10, -44, 20, -5,-3]
+print("For List1: ")
+for num in list1:
+    if num >= 0:
+        print(num, end= " ")
+
+list2 = [-11, -24, 66, -45,93]
+print("\nFor List2: ")
+for num in list2:   
+    if num < 0:
+        print(num, end= " ")
