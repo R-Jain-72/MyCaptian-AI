f31482aefdbca1fb1df4012c11ffb4a5ce1a37b0
R-Jain-72 <129484277+R-Jain-72@users.noreply.github.com>
2023-05-28 11:13:43 +0530

    Add files via upload

---
+++ Task04-Set-Operations.py
@@ -0,0 +1,10 @@
+E = {2, 4, 6, 8, 10, 12, 16};
+N = {10, 12, 13, 4, 8};
+
+print("Union of E and N is",E | N)
+
+print("Intersection of E and N is",E & N)
+
+print("Difference of E and N is",E - N)
+
+print("Symmetric difference of E and N is",E ^ N)
